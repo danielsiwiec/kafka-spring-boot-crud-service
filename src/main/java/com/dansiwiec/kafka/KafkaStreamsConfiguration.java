51804f3810f363bf7f39fc76d888c9c4616032eb
Dan Siwiec <daniel.siwiec@gmail.com>
2020-04-05 19:13:36 -0700

    Replace deprecated method invocation

--- src/main/java/com/dansiwiec/kafka/KafkaStreamsConfiguration.java
+++ src/main/java/com/dansiwiec/kafka/KafkaStreamsConfiguration.java
@@ -1,11 +1,10 @@
 package com.dansiwiec.kafka;
 
 import com.dansiwiec.models.Todo;
-import java.util.Map;
+import java.util.Properties;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -19,15 +18,11 @@ import org.springframework.kafka.support.serializer.JsonSerde;
 public class KafkaStreamsConfiguration {
 
   @Bean
-  public StreamsConfig streamsConfig(KafkaProperties kafkaProperties) {
-    Map<String, Object> streamProps = kafkaProperties.buildStreamsProperties();
-    return new StreamsConfig(streamProps);
-  }
-
-  @Bean
-  public KafkaStreams kafkaStreams(StreamsBuilder streamsBuilder, StreamsConfig streamsConfig) {
+  public KafkaStreams kafkaStreams(StreamsBuilder streamsBuilder, KafkaProperties kafkaProperties) {
     streamsBuilder.table(Topics.TODO, Consumed.with(Serdes.String(), new JsonSerde<>(Todo.class)), Materialized.as("todos"));
-    KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), streamsConfig);
+    Properties streamProperties = new Properties();
+    streamProperties.putAll(kafkaProperties.buildStreamsProperties());
+    KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), streamProperties);
     streams.cleanUp();
     streams.start();
     return streams;
